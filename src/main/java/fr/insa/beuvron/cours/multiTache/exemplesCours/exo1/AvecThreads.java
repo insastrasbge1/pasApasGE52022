67a66566f0f393995837ed53fc096d5201112d12
francois <francois@PortableFdB>
2022-09-16 14:32:00 +0200

    fin de cours 1

--- src/main/java/fr/insa/beuvron/cours/multiTache/exemplesCours/exo1/AvecThreads.java
+++ src/main/java/fr/insa/beuvron/cours/multiTache/exemplesCours/exo1/AvecThreads.java
@@ -16,7 +16,6 @@
     You should have received a copy of the GNU General Public License
     along with CoursBeuvron.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package fr.insa.beuvron.cours.multiTache.exemplesCours.exo1;
 
 import java.util.logging.Level;
@@ -27,5 +26,55 @@ import java.util.logging.Logger;
  * @author francois
  */
 public class AvecThreads {
-    // TODO
+
+    public static class Pair extends Thread {
+
+        @Override
+        public void run() {
+            for (int i = 0; i <= 10; i = i + 2) {
+                System.out.println(i);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    throw new Error("ne devrait jamais arriver");
+                }
+            }
+
+        }
+    }
+
+    public static class Impair extends Thread {
+
+        @Override
+        public void run() {
+            for (int i = 1; i <= 10; i = i + 2) {
+                System.out.println(i);
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException ex) {
+                    throw new Error("ne devrait jamais arriver");
+                }
+            }
+
+        }
+    }
+
+    public static void parallele() {
+        Thread pair = new Pair();
+        Thread impair = new Impair();
+        pair.start();
+        impair.start();
+        try {
+            pair.join();
+            impair.join();
+        } catch (InterruptedException ex) {
+            throw new Error("ne devrait jamais arriver");
+        }
+        System.out.println("c'est fini");
+    }
+
+    public static void main(String[] args) {
+        parallele();
+    }
+
 }
